e924ebf305ee69e7f24a4882004c4201b60eaf25
stratolark <69923983+stratolark@users.noreply.github.com>
2026-06-06 17:57:11 -0400

    feat: add getopt to parse and validate command options

--- ls.c
+++ ls.c
@@ -28,10 +28,8 @@
 // int argc = means the argument count, how many words when they ran the program
 // char *argv[] = a pointer to a pointer of an array of strings, in C strings are an array of chars
 int main(int argc, char *argv[]){
-    printf("hello from world \n");
     // print: take the first argument, which is always the name of the program
     const char *path = (argc > 1) ? argv[1] : ".";
-    printf("%s \n", path);
 
     // DIR is an opaque type, we dont need to know what it is, but the systems knows what it is and knows how to use it.
     // It represents a directory type.

fb16471f5cfdc177808df96782074612d581cad3
stratolark <69923983+stratolark@users.noreply.github.com>
2026-06-06 16:26:35 -0400

    feat: add new type of main fn

--- ls.c
+++ ls.c
@@ -1,7 +1,19 @@
 
 // main function that takes no arguments, returns an int. 0 = success
+// #include <stdio.h>
+// int main(void){
+//     printf("hello from world");
+//     return 0;
+// }
+
 #include <stdio.h>
-int main(void){
-    printf("hello from world");
+
+// to take parameters we need to change it from void to int argc, char *argv[]
+// int argc = means the argument count, how many words when they ran the program
+// char *argv[] = a pointer to a pointer of an array of strings, in C strings are an array of chars
+int main(int argc, char *argv[]){
+    printf("hello from world \n");
+    // print: take the first argument, which is always the name of the program
+    printf("%s \n", argv[0]);
     return 0;
 }
